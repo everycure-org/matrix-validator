11afc9f9ff0e35ad2277b956f06ed2162d92588a
Jason Reilly <jdr0887@gmail.com>
2025-01-23 02:24:25 -0500

    adding data checks for edges/nodes

--- src/matrix_validator/validator.py
+++ src/matrix_validator/validator.py
@@ -1,5 +1,7 @@
 """Main python file."""
 
+import polars as pl
+import pandera.polars as pa
 import logging
 
 from matrix_validator.datamodels import EdgeSchema, NodeSchema
@@ -26,6 +28,58 @@ def validate_kg(nodes, edges, output_format, report_file):
         edges_df = read_tsv_as_strings(edges)
         logger.debug(f"Edges DataFrame: {edges_df.head()}")
         edge_validation = EdgeSchema.validate(edges_df)
+        validation_reports.append(f"Edges Validation Passed: {edge_validation}")
+
+    # Write validation report
+    write_report(output_format, report_file, validation_reports)
+    logging.info(f"Validation report written to {report_file}")
+
+
+def validate_kg_nodes(nodes, output_format, report_file):
+    """Validate a knowledge graph using optional nodes and edges TSV files."""
+    validation_reports = []
+
+    # Validate nodes if provided
+    if nodes:
+        logger.info("Validating nodes TSV...")
+
+        schema = pa.DataFrameSchema({
+            "id": pa.Column(str, [
+                pa.Check.str_matches(r'^[A-Za-z_]+:.+$', raise_warning=True),
+            ]),
+            "category": pa.Column(str, [
+                pa.Check.str_matches(r'^biolink:.+$', raise_warning=True),
+            ]),
+        })
+
+        node_validation = pl.scan_csv(nodes, separator="\t").collect().pipe(schema.validate)
+        validation_reports.append(f"Nodes Validation Passed: {node_validation}")
+
+    # Write validation report
+    write_report(output_format, report_file, validation_reports)
+    logging.info(f"Validation report written to {report_file}")
+
+def validate_kg_edges(edges, output_format, report_file):
+    """Validate a knowledge graph using optional nodes and edges TSV files."""
+    validation_reports = []
+
+    # Validate edges if provided
+    if edges:
+        logger.info("Validating edges TSV...")
+
+        schema = pa.DataFrameSchema({
+            "subject": pa.Column(str, [
+                pa.Check.str_matches(r'^[A-Za-z_]+:.+$', raise_warning=True),
+            ]),
+            "predicate": pa.Column(str, [
+                pa.Check.str_matches(r'^biolink:.+$', raise_warning=True),
+            ]),
+            "object": pa.Column(str, [
+                pa.Check.str_matches(r'^[A-Za-z_]+:.+$', raise_warning=True),
+            ]),
+        })
+
+        edge_validation = pl.scan_csv(edges, separator="\t").collect().pipe(schema.validate)
         validation_reports.append(f"Edges Validation Passed: {edge_validation}")
 
     # Write validation report
