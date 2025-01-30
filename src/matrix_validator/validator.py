b3059cb42d3cf6102ee2d11f1593b165bea9a25d
Jason Reilly <jdr0887@gmail.com>
2025-01-29 22:34:59 -0500

    adding validate_kg_edges & validate_kg_nodes

--- src/matrix_validator/validator.py
+++ src/matrix_validator/validator.py
@@ -1,7 +1,7 @@
 """Main python file."""
 
 import logging
-
+import polars as pl
 from matrix_validator.datamodels import EdgeSchema, NodeSchema
 from matrix_validator.util import read_tsv_as_strings
 
@@ -30,6 +30,53 @@ def validate_kg(nodes, edges, output_format, report_file):
 
     # Write validation report
     write_report(output_format, report_file, validation_reports)
+    logging.info(f"Validation report written to {report_file}")
+
+
+def validate_kg_nodes(nodes, output_format, report_file):
+    """Validate a knowledge graph using optional nodes TSV files."""
+    validation_reports = []
+
+    # Validate nodes if provided
+    if nodes:
+        logger.info("Validating nodes TSV...")
+            
+        curie_regex = "^[A-Za-z_]+:.+$"
+        starts_with_biolink_regex = "^biolink:.+$"
+
+        validation_reports = pl.scan_csv(nodes, separator='\t', truncate_ragged_lines=True, has_header=True, ignore_errors=True).select([
+            pl.col("id").str.contains(curie_regex).sum().alias("valid_curie_id_count"),
+            (~pl.col("id").str.contains(curie_regex)).sum().alias("invalid_curie_id_count"),
+            pl.col("category").str.contains(starts_with_biolink_regex).sum().alias("valid_starts_with_biolink_category_count"),
+            (~pl.col("category").str.contains(starts_with_biolink_regex)).sum().alias("invalid_starts_with_biolink_category_count")
+        ]).collect()
+
+    # Write validation report
+    validation_reports.write_json(report_file)
+    logging.info(f"Validation report written to {report_file}")
+
+def validate_kg_edges(edges, output_format, report_file):
+    """Validate a knowledge graph using optional edges TSV files."""
+    validation_reports = []
+
+    # Validate nodes if provided
+    if edges:
+        logger.info("Validating edges TSV...")
+
+        curie_regex = "^[A-Za-z_]+:.+$"
+        starts_with_biolink_regex = "^biolink:.+$"
+
+        validation_reports = pl.scan_csv(edges, separator='\t', truncate_ragged_lines=True, has_header=True, ignore_errors=True).select([
+            pl.col("subject").str.contains(curie_regex).sum().alias("valid_curie_subject_count"),
+            (~pl.col("subject").str.contains(curie_regex)).sum().alias("invalid_curie_subject_count"),
+            pl.col("predicate").str.contains(starts_with_biolink_regex).sum().alias("valid_starts_with_biolink_predicate_count"),
+            (~pl.col("predicate").str.contains(starts_with_biolink_regex)).sum().alias("invalid_starts_with_biolink_predicate_count"),
+            pl.col("object").str.contains(curie_regex).sum().alias("valid_curie_object_count"),
+            (~pl.col("object").str.contains(curie_regex)).sum().alias("invalid_curie_object_count"),
+        ]).collect()
+
+    # Write validation report
+    validation_reports.write_json(report_file)
     logging.info(f"Validation report written to {report_file}")
 
 
