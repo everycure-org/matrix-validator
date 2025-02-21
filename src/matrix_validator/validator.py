ce15925a0b390638222882baa109f7a5f590a667
Nico Matentzoglu <nicolas.matentzoglu@gmail.com>
2025-02-21 11:25:49 +0200

    Remove some unnecessary lines...
    ... I accidentally introduced during the refactor

--- src/matrix_validator/validator.py
+++ src/matrix_validator/validator.py
@@ -3,12 +3,6 @@
 import os
 from abc import ABC, abstractmethod
 
-NCNAME_PATTERN = r"[A-Za-z_][A-Za-z0-9\.\-_]*"
-LOCAL_UNIQUE_IDENTIFIER_PATTERN = r"(/[^\s/][^\s]*|[^\s/][^\s]*|[^\s]?)"
-
-CURIE_REGEX = rf"^({NCNAME_PATTERN}?:)?{LOCAL_UNIQUE_IDENTIFIER_PATTERN}$"
-STARTS_WITH_BIOLINK_REGEX = rf"^biolink:{LOCAL_UNIQUE_IDENTIFIER_PATTERN}$"
-
 
 class Validator(ABC):
     """Abstract class for a validator."""
