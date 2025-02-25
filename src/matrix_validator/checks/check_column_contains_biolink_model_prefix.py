835b7a21ee0cad1519d2a081dcd9661542179ce0
Jason Reilly <jdr0887@gmail.com>
2025-02-25 11:16:18 -0500

    formatting

--- src/matrix_validator/checks/check_column_contains_biolink_model_prefix.py
+++ src/matrix_validator/checks/check_column_contains_biolink_model_prefix.py
@@ -4,6 +4,7 @@ import polars as pl
 
 from matrix_validator.checks import DELIMITED_BY_PIPES
 
+
 def validate(column, bm_prefixes: list, file):
     """Validate contains Biolink Model prefix."""
     violations_df = (
