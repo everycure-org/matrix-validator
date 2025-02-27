2bba172264749654318409a9ab07bb1e9bcc8fd4
Jason Reilly <jdr0887@gmail.com>
2025-02-27 09:48:32 -0500

    initial commit

---
+++ src/matrix_validator/checks/check_edge_ids_in_node_ids.py
@@ -0,0 +1,22 @@
+"""Polars-based validator check."""
+
+import polars as pl
+
+
+def validate(edge_ids: list, file):
+    """Validate contains Edge subject/object exist in Nodes."""
+    column = "id"
+    violations_df = (
+        pl.scan_csv(file, separator="\t", truncate_ragged_lines=True, has_header=True, ignore_errors=True)
+        .select(
+            [
+                pl.when(~pl.col(column).str.contains_any(edge_ids))
+                .then(pl.col(column))
+                .otherwise(pl.lit(None))
+                .alias(f"invalid_edge_ids_in_node_ids"),
+            ]
+        )
+        .filter(pl.col(f"invalid_edge_ids_in_node_ids").is_not_null())
+        .collect()
+    )
+    return violations_df.write_ndjson()
