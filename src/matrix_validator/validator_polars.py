5b2f53e9bec4a54db1abffbc4ba8d64b1c92f9ee
Jason Reilly <jdr0887@gmail.com>
2025-02-27 09:51:27 -0500

    adding check to see if any edge subject/object are not in the nodes file

--- src/matrix_validator/validator_polars.py
+++ src/matrix_validator/validator_polars.py
@@ -21,6 +21,7 @@ from matrix_validator.checks.check_column_contains_biolink_model_agent_type impo
     validate as check_column_contains_biolink_model_agent_type,
 )
 from matrix_validator.checks.check_column_is_valid_curie import validate as check_column_is_valid_curie
+from matrix_validator.checks.check_edge_ids_in_node_ids import validate as check_edge_ids_in_node_ids
 from matrix_validator.checks.check_column_contains_biolink_model_prefix import validate as check_column_contains_biolink_model_prefix
 from matrix_validator.checks.check_column_no_leading_whitespace import validate as check_column_no_leading_whitespace
 from matrix_validator.checks.check_column_no_trailing_whitespace import validate as check_column_no_trailing_whitespace
@@ -38,40 +39,50 @@ class ValidatorPolarsImpl(Validator):
         """Create a new instance of the polars-based validator."""
         super().__init__()
 
-    def validate(self, nodes_file_path, edges_file_file_path):
+    def validate(self, nodes_file_path, edges_file_path):
         """Validate a knowledge graph as nodes and edges KGX TSV files."""
+        validation_reports = []
+
         if nodes_file_path:
-            validate_kg_nodes(nodes_file_path, self.output_format, self.get_report_file())
+            validation_reports.extend(validate_kg_nodes(nodes_file_path))
 
-        if edges_file_file_path:
-            validate_kg_edges(edges_file_file_path, self.output_format, self.get_report_file())
+        if edges_file_path:
+            validation_reports.extend(validate_kg_edges(edges_file_path))
 
+        if nodes_file_path and edges_file_path:
+            validation_reports.extend(validate_node_ids_in_edges(nodes_file_path, edges_file_path))
 
-def get_biolink_model_prefix_keys():
-    from . import resources
-
-    with il_resources.open_text(resources, "biolink-model.yaml") as file:
-        bl_model_data = list(yaml.load_all(file, Loader=SafeLoader))
-    return list(bl_model_data[0]["prefixes"].keys())
-
-
-def get_biolink_model_knowledge_level_keys():
-    from . import resources
-
-    with il_resources.open_text(resources, "biolink-model.yaml") as file:
-        bl_model_data = list(yaml.load_all(file, Loader=SafeLoader))
-    return list(bl_model_data[0]["enums"]["KnowledgeLevelEnum"]["permissible_values"].keys())
+        # Write validation report
+        write_report(self.output_format, self.get_report_file(), validation_reports)
+        logging.info(f"Validation report written to {self.get_report_file()}")
 
 
-def get_biolink_model_agent_type_keys():
-    from . import resources
+def validate_node_ids_in_edges(nodes, edges):
+    """Validate a knowledge graph nodes vs edges."""
+    logger.info("Validating nodes & edges")
 
-    with il_resources.open_text(resources, "biolink-model.yaml") as file:
-        bl_model_data = list(yaml.load_all(file, Loader=SafeLoader))
-    return list(bl_model_data[0]["enums"]["AgentTypeEnum"]["permissible_values"].keys())
+    edges_df = (
+        pl.scan_csv(edges, separator="\t", truncate_ragged_lines=True, has_header=True, ignore_errors=True)
+        .select([pl.col("subject"), pl.col("object")])
+        .collect()
+    )
+    edge_ids = pl.concat([edges_df.select(pl.col("subject").alias("id")), edges_df.select(pl.col("object").alias("id"))]).get_column("id")
+
+    counts_df = (
+        pl.scan_csv(nodes, separator="\t", truncate_ragged_lines=True, has_header=True, ignore_errors=True)
+        .select([(~pl.col("id").str.contains_any(edge_ids)).sum().alias("invalid_edge_ids_in_node_ids_count")])
+        .collect()
+    )
+
+    validation_reports = []
+
+    if counts_df.get_column("invalid_edge_ids_in_node_ids_count").item(0) > 0:
+        validation_reports.append(check_edge_ids_in_node_ids(edge_ids, nodes))
+
+    return validation_reports
 
 
-def validate_kg_nodes(nodes, output_format, report_file):
+def validate_kg_nodes(nodes):
     """Validate a knowledge graph using optional nodes TSV files."""
     logger.info("Validating nodes TSV...")
 
@@ -81,24 +92,17 @@ def validate_kg_nodes(nodes, output_format, report_file):
         pl.scan_csv(nodes, separator="\t", truncate_ragged_lines=True, has_header=True, ignore_errors=True)
         .select(
             [
-                # we only really care about the invalids
-                # pl.col("id").str.contains(CURIE_REGEX).sum().alias("valid_curie_id_count"),
                 (~pl.col("id").str.contains(CURIE_REGEX)).sum().alias("invalid_curie_id_count"),
-                # pl.col("id").str.contains_any(bm_prefixes).sum().alias("valid_contains_biolink_model_prefix_id_count"),
                 (~pl.col("id").str.contains_any(bm_prefixes)).sum().alias("invalid_contains_biolink_model_prefix_id_count"),
-                # pl.col("category").str.contains(STARTS_WITH_BIOLINK_REGEX).sum().alias("valid_starts_with_biolink_category_count"),
                 (~pl.col("category").str.contains(STARTS_WITH_BIOLINK_REGEX)).sum().alias("invalid_starts_with_biolink_category_count"),
-                # pl.col("category").str.contains(DELIMITED_BY_PIPES).sum().alias("valid_delimited_by_pipes_category_count"),
                 (~pl.col("category").str.contains(DELIMITED_BY_PIPES)).sum().alias("invalid_delimited_by_pipes_category_count"),
-                # pl.col("category").str.contains(NO_LEADING_WHITESPACE).sum().alias("valid_no_leading_whitespace_category_count"),
                 (~pl.col("category").str.contains(NO_LEADING_WHITESPACE)).sum().alias("invalid_no_leading_whitespace_category_count"),
-                # pl.col("category").str.contains(NO_TRAILING_WHITESPACE).sum().alias("valid_no_trailing_whitespace_category_count"),
                 (~pl.col("category").str.contains(NO_TRAILING_WHITESPACE)).sum().alias("invalid_no_trailing_whitespace_category_count"),
             ]
         )
         .collect()
     )
-    # print(counts_df.write_json())
+
     validation_reports = []
 
     if counts_df.get_column("invalid_curie_id_count").item(0) > 0:
@@ -119,12 +123,10 @@ def validate_kg_nodes(nodes, output_format, report_file):
     if counts_df.get_column("invalid_delimited_by_pipes_category_count").item(0) > 0:
         validation_reports.append(check_column_is_delimited_by_pipes("category", nodes))
 
-    # Write validation report
-    write_report(output_format, report_file, validation_reports)
-    logging.info(f"Validation report written to {report_file}")
+    return validation_reports
 
 
-def validate_kg_edges(edges, output_format, report_file):
+def validate_kg_edges(edges):
     """Validate a knowledge graph using optional edges TSV files."""
     logger.info("Validating edges TSV...")
 
@@ -136,22 +138,14 @@ def validate_kg_edges(edges, output_format, report_file):
         pl.scan_csv(edges, separator="\t", truncate_ragged_lines=True, has_header=True, ignore_errors=True)
         .select(
             [
-                # we only really care about the invalids
-                # pl.col("subject").str.contains(CURIE_REGEX).sum().alias("valid_curie_subject_count"),
                 (~pl.col("subject").str.contains(CURIE_REGEX)).sum().alias("invalid_curie_subject_count"),
-                # pl.col("subject").str.contains_any(bm_prefixes).sum().alias("valid_contains_biolink_model_prefix_subject_count"),
                 (~pl.col("subject").str.contains_any(bm_prefixes)).sum().alias("invalid_contains_biolink_model_prefix_subject_count"),
-                # pl.col("predicate").str.contains(STARTS_WITH_BIOLINK_REGEX).sum().alias("valid_starts_with_biolink_predicate_count"),
                 (~pl.col("predicate").str.contains(STARTS_WITH_BIOLINK_REGEX)).sum().alias("invalid_starts_with_biolink_predicate_count"),
-                # pl.col("object").str.contains(CURIE_REGEX).sum().alias("valid_curie_object_count"),
                 (~pl.col("object").str.contains(CURIE_REGEX)).sum().alias("invalid_curie_object_count"),
-                # pl.col("object").str.contains_any(bm_prefixes).sum().alias("valid_contains_biolink_model_prefix_object_count"),
                 (~pl.col("object").str.contains_any(bm_prefixes)).sum().alias("invalid_contains_biolink_model_prefix_object_count"),
-                # pl.col("knowledge_level").str.contains_any(bm_knowledge_level_keys).sum().alias("valid_contains_biolink_model_knowledge_level_count"),
                 (~pl.col("knowledge_level").str.contains_any(bm_knowledge_level_keys))
                 .sum()
                 .alias("invalid_contains_biolink_model_knowledge_level_count"),
-                # pl.col("agent_type").str.contains_any(bm_knowledge_level_keys).sum().alias("valid_contains_biolink_model_agent_type_count"),
                 (~pl.col("agent_type").str.contains_any(bm_knowledge_level_keys))
                 .sum()
                 .alias("invalid_contains_biolink_model_agent_type_count"),
@@ -183,9 +177,7 @@ def validate_kg_edges(edges, output_format, report_file):
     if counts_df.get_column("invalid_contains_biolink_model_agent_type_count").item(0) > 0:
         validation_reports.append(check_column_contains_biolink_model_agent_type("agent_type", bm_agent_type_keys, edges))
 
-    # Write validation report
-    write_report(output_format, report_file, validation_reports)
-    logging.info(f"Validation report written to {report_file}")
+    return validation_reports
 
 
 def write_report(output_format, report_file, validation_reports):
@@ -196,3 +188,27 @@ def write_report(output_format, report_file, validation_reports):
                 report.write("\n".join(validation_reports))
             elif output_format == "md":
                 report.write("\n\n".join([f"## {line}" for line in validation_reports]))
+
+
+def get_biolink_model_prefix_keys():
+    from . import resources
+
+    with il_resources.open_text(resources, "biolink-model.yaml") as file:
+        bl_model_data = list(yaml.load_all(file, Loader=SafeLoader))
+    return list(bl_model_data[0]["prefixes"].keys())
+
+
+def get_biolink_model_knowledge_level_keys():
+    from . import resources
+
+    with il_resources.open_text(resources, "biolink-model.yaml") as file:
+        bl_model_data = list(yaml.load_all(file, Loader=SafeLoader))
+    return list(bl_model_data[0]["enums"]["KnowledgeLevelEnum"]["permissible_values"].keys())
+
+
+def get_biolink_model_agent_type_keys():
+    from . import resources
+
+    with il_resources.open_text(resources, "biolink-model.yaml") as file:
+        bl_model_data = list(yaml.load_all(file, Loader=SafeLoader))
+    return list(bl_model_data[0]["enums"]["AgentTypeEnum"]["permissible_values"].keys())
