dc4592a64f8b4c7a0b320be1aab8acc4f29332da
Jason Reilly <jdr0887@gmail.com>
2025-03-04 10:27:13 -0500

    reverting

--- src/matrix_validator/checks/check_edge_ids_in_node_ids.py
+++ src/matrix_validator/checks/check_edge_ids_in_node_ids.py
@@ -3,15 +3,20 @@
 import polars as pl
 
 
-def validate(df, edge_ids: list):
+def validate(edge_ids: list, file):
     """Validate contains Edge subject/object exist in Nodes."""
     column = "id"
-    violations_df = df.select(
-        [
-            pl.when(~pl.col(column).str.contains_any(edge_ids))
-            .then(pl.col(column))
-            .otherwise(pl.lit(None))
-            .alias("invalid_edge_ids_in_node_ids"),
-        ]
-    ).filter(pl.col("invalid_edge_ids_in_node_ids").is_not_null())
+    violations_df = (
+        pl.scan_csv(file, separator="\t", truncate_ragged_lines=True, has_header=True, ignore_errors=True)
+        .select(
+            [
+                pl.when(~pl.col(column).str.contains_any(edge_ids))
+                .then(pl.col(column))
+                .otherwise(pl.lit(None))
+                .alias("invalid_edge_ids_in_node_ids"),
+            ]
+        )
+        .filter(pl.col("invalid_edge_ids_in_node_ids").is_not_null())
+        .collect()
+    )
     return violations_df.write_ndjson()
