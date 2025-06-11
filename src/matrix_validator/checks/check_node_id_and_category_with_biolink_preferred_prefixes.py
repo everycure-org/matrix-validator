ed1ad5f9a984e46f050c344b745a8394e7daf9a6
Jason Reilly <jdr0887@gmail.com>
2025-06-11 11:50:24 -0400

    initial commit

---
+++ src/matrix_validator/checks/check_node_id_and_category_with_biolink_preferred_prefixes.py
@@ -0,0 +1,30 @@
+"""Polars-based validator check."""
+
+import polars as pl
+
+def validate(class_prefix_map, df):
+    """Validate node.id and category align with biolink preferred prefix mappings."""
+
+    results = {}
+
+    for row in df.rows(named=True):
+        prefix = row["id"].split(":")[0]
+        row_category = row["category"]
+        if "|" in row_category:
+            cat = row_category.split("|")[0]
+        else:
+            cat = row_category
+
+        if not cat in class_prefix_map.keys():
+            continue
+
+        if not prefix in class_prefix_map[cat]:
+            if cat not in results:
+                results[cat] = set()
+            results[cat].add(prefix)
+
+    violations = set()
+    for key, value in results.items():
+        violations.add(f"The prefixes {value} are not within the Biolink preferred category mapping of '{key}'")
+
+    return violations
