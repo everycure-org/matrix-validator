037389fd876d99e357a867ae39117ad9263bea1f
Jason Reilly <jdr0887@gmail.com>
2025-07-28 15:02:00 -0400

    adding 2 more tests

--- tests/test_validate.py
+++ tests/test_validate.py
@@ -42,23 +42,45 @@ class TestValidate(unittest.TestCase):
 
         print(prefix_class_map)
 
-    def test_validate_dataframe_impl(self):
+    def test_validate_dataframe_rtxkg2(self):
         """Test validation method from DataFrame implementation."""
-        test_nodes = os.path.join("tests/data/testdata_robokop-kg_nodes.tsv")
-        nodes_df = pl.scan_csv(test_nodes, separator="\t", has_header=True, ignore_errors=True).limit(10).collect()
-        test_edges = os.path.join("tests/data/testdata_robokop-kg_edges.tsv")
-        edges_df = pl.scan_csv(test_edges, separator="\t", has_header=True, ignore_errors=True).limit(10).collect()
+        test_nodes = os.path.join("tests/data/testdata_rtxkg2-kg_nodes.tsv")
+        nodes_df = pl.scan_csv(test_nodes, separator="\t", has_header=True, ignore_errors=True).collect()
+        test_edges = os.path.join("tests/data/testdata_rtxkg2-kg_edges.tsv")
+        edges_df = pl.scan_csv(test_edges, separator="\t", has_header=True, ignore_errors=True).collect()
         validator = ValidatorPolarsDataFrameImpl(nodes=nodes_df, edges=edges_df)
         ret = validator.validate()
         print("\n".join(validator.violations))
-
         self.assertTrue(ret == 1)
 
-    def test_validate_files_impl(self):
+    def test_validate_dataframe_robokop(self):
+        """Test validation method from DataFrame implementation."""
+        # robokop
+        test_nodes = os.path.join("tests/data/testdata_robokop-kg_nodes.tsv")
+        nodes_df = pl.scan_csv(test_nodes, separator="\t", has_header=True, ignore_errors=True).collect()
+        test_edges = os.path.join("tests/data/testdata_robokop-kg_edges.tsv")
+        edges_df = pl.scan_csv(test_edges, separator="\t", has_header=True, ignore_errors=True).collect()
+        validator = ValidatorPolarsDataFrameImpl(nodes=nodes_df, edges=edges_df)
+        ret = validator.validate()
+        print("\n".join(validator.violations))
+        self.assertTrue(ret == 1)
+
+    def test_validate_file_rtxkg2(self):
         """Test validation method from DataFrame implementation."""
         # matrix_data_dir = ""
-        # test_nodes = os.path.join(dir, "data/test/raw/KGs/robokop-kg/30fd1bfc18cd5ccb/robokop-30fd1bfc18cd5ccb_nodes.tsv")
-        # test_edges = os.path.join(dir, "data/test/raw/KGs/robokop-kg/30fd1bfc18cd5ccb/robokop-30fd1bfc18cd5ccb_edges.tsv")
+        # test_nodes = os.path.join(matrix_data_dir, "data/01_RAW/KGs/rtx_kg2/v2.10.0_validated.1/rtx-kg2_2.10.0_v2-2_nodes_100.tsv")
+        # test_edges = os.path.join(matrix_data_dir, "data/01_RAW/KGs/rtx_kg2/v2.10.0_validated.1/rtx-kg2_2.10.0_v2-2_edges_100.tsv")
+        test_nodes = os.path.join("tests/data/testdata_rtxkg2-kg_nodes.tsv")
+        test_edges = os.path.join("tests/data/testdata_rtxkg2-kg_edges.tsv")
+        validator = ValidatorPolarsFileImpl(nodes_file_path=test_nodes, edges_file_path=test_edges)
+
+        self.assertTrue(validator.validate() == 1)
+
+    def test_validate_file_robokop(self):
+        """Test validation method from DataFrame implementation."""
+        # matrix_data_dir = ""
+        # test_nodes = os.path.join(matrix_data_dir, "data/test/raw/KGs/robokop-kg/30fd1bfc18cd5ccb/robokop-30fd1bfc18cd5ccb_nodes.tsv")
+        # test_edges = os.path.join(matrix_data_dir, "data/test/raw/KGs/robokop-kg/30fd1bfc18cd5ccb/robokop-30fd1bfc18cd5ccb_edges.tsv")
         test_nodes = os.path.join("tests/data/testdata_robokop-kg_nodes.tsv")
         test_edges = os.path.join("tests/data/testdata_robokop-kg_edges.tsv")
         validator = ValidatorPolarsFileImpl(nodes_file_path=test_nodes, edges_file_path=test_edges)
